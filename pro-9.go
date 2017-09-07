0b8e6b6888fb5ce1bcf223e691b357c93484718d
oldbeyond <dl33_wjlu@126.com>
2017-09-07 13:27:29 +0800

    add pro-8

---
+++ pro-9.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	var a, b, c int
+	for a = 1; a < 1001; a++ {
+		for b = 1; b < 1001; b++ {
+			for c = 1; c < 1001; c++ {
+				if a + b + c == 1000 {
+					if a * a + b * b == c * c {
+						fmt.Println(a)
+						fmt.Println(b)
+						fmt.Println(c)
+						fmt.Println(a * b * c)
+						return
+					}
+				}
+			}
+		}
+	}
+}
